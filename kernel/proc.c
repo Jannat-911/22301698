28759e00e0a8a2704af2e3fe5521dca3cb0ea94e
Jannat <miftahul8889@gmail.com>
2026-05-08 23:48:47 +0600

    Fix folder structure

---
+++ kernel/proc.c
@@ -0,0 +1,604 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "mmu.h"
+#include "x86.h"
+#include "proc.h"
+#define PHI 0x9e3779b9
+#define STORE_TICKETS_ON_SLEEP
+
+//page table init.
+struct ptable_type ptable = {{0}};
+
+static struct proc *initproc;
+
+int nextpid = 1;
+extern void forkret(void);
+extern void trapret(void);
+
+static void wakeup1(void *chan);
+
+void
+pinit(void)
+{
+  initlock(&ptable.lock, "ptable");
+}
+
+
+/* The following code is added by haoda le and netid hxl180046 
+**Random function. 
+**create a random number to pick the tickets for next running process
+*/
+/* Pseudorandom generator added for Lottery Scheduler Task 3 */
+unsigned long rand_next = 1;
+
+int
+do_rand(unsigned long *ctx)
+{
+  long hi, lo, x;
+  x = (*ctx % 0x7ffffffe) + 1;
+  hi = x / 127773;
+  lo = x % 127773;
+  x = 16807 * lo - 2836 * hi;
+  if (x < 0)
+    x += 0x7fffffff;
+  *ctx = x;
+  return (x);
+}
+
+int
+rand(void)
+{
+  return (do_rand(&rand_next));
+}
+/* End of Pseudorandom generator */
+/* End of code added*/
+
+
+//keep track of the amount of tickets handed out 
+int total_tickets;
+
+/* The following code is added by haoda le and netid hxl180046
+**Set process tickets.
+**This function should always  be in a lock 
+*/
+void setproctickets(struct proc* pp, int n)
+{
+  total_tickets -= pp->tickets;
+  pp->tickets = n;
+  total_tickets += pp->tickets;
+}
+/* End of code added*/
+
+
+/* The following code is added by haoda le and netid hxl180046
+**store tickets. just after sleeping
+*/
+void storetickets(struct proc* pp)
+{
+  if(pp->state != SLEEPING)
+    panic("Not sleeping at storetickets");
+#ifdef STORE_TICKETS_ON_SLEEP
+  total_tickets -= pp->tickets;
+#endif
+}
+/* End of code added*/
+
+
+/* The following code is added by haoda le and netid hxl180046
+**restore tickets. just before waking
+*/
+void restoretickets(struct proc* pp)
+{
+  if(pp->state != SLEEPING)
+    panic("Not sleeping at waketickets");
+#ifdef STORE_TICKETS_ON_SLEEP
+  total_tickets += pp->tickets;
+#endif
+}
+/* End of code added*/
+
+
+// Look in the process table for an UNUSED proc.
+// If found, change state to EMBRYO and initialize
+// state required to run in the kernel.
+// Otherwise return 0.
+static struct proc*
+allocproc(void)
+{
+  struct proc *p;
+  char *sp;
+
+  acquire(&ptable.lock);
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    if(p->state == UNUSED)
+      goto found;
+  release(&ptable.lock);
+  return 0;
+
+ found:
+  p->state = EMBRYO;
+  p->pid = nextpid++;
+  p->rounds = 0;       // <--- এই লাইনটি এখানে যোগ করবেন
+  release(&ptable.lock);
+
+  // Allocate kernel stack if possible.
+  if((p->kstack = kalloc()) == 0){
+    p->state = UNUSED;
+    return 0;
+  }
+  sp = p->kstack + KSTACKSIZE;
+  
+  // Leave room for trap frame.
+  sp -= sizeof *p->tf;
+  p->tf = (struct trapframe*)sp;
+  
+  // Set up new context to start executing at forkret,
+  // which returns to trapret.
+  sp -= 4;
+  *(uint*)sp = (uint)trapret;
+
+  sp -= sizeof *p->context;
+  p->context = (struct context*)sp;
+  memset(p->context, 0, sizeof *p->context);
+  p->context->eip = (uint)forkret;
+
+  return p;
+}
+
+// Set up first user process.
+// Set up first user process.
+void
+userinit(void)
+{
+  struct proc *p;
+  extern char _binary_initcode_start[], _binary_initcode_size[];
+  
+  p = allocproc();
+  acquire(&ptable.lock);
+  initproc = p;
+  if((p->pgdir = setupkvm()) == 0)
+    panic("userinit: out of memory?");
+  inituvm(p->pgdir, _binary_initcode_start, (int)_binary_initcode_size);
+  p->sz = PGSIZE;
+  memset(p->tf, 0, sizeof(*p->tf));
+  p->tf->cs = (SEG_UCODE << 3) | DPL_USER;
+  p->tf->ds = (SEG_UDATA << 3) | DPL_USER;
+  p->tf->es = p->tf->ds;
+  p->tf->ss = p->tf->ds;
+  p->tf->eflags = FL_IF;
+  p->tf->esp = PGSIZE;
+  p->tf->eip = 0;  // beginning of initcode.S
+
+  safestrcpy(p->name, "initcode", sizeof(p->name));
+  p->cwd = namei("/");
+
+  p->tickets = 10;       // <--- এই লাইনটি এখানে যোগ করবেন
+  p->state = RUNNABLE;
+  release(&ptable.lock);
+}
+// Grow current process's memory by n bytes.
+// Return 0 on success, -1 on failure.
+int
+growproc(int n)
+{
+  uint sz;
+  
+  sz = proc->sz;
+  if(n > 0){
+    if((sz = allocuvm(proc->pgdir, sz, sz + n)) == 0)
+      return -1;
+  } else if(n < 0){
+    if((sz = deallocuvm(proc->pgdir, sz, sz + n)) == 0)
+      return -1;
+  }
+  proc->sz = sz;
+  switchuvm(proc);
+  return 0;
+}
+
+// Create a new process copying p as the parent.
+// Sets up stack to return as if from system call.
+// Caller must set state of returned proc to RUNNABLE.
+// Create a new process copying p as the parent.
+// Sets up stack to return as if from system call.
+// Caller must set state of returned proc to RUNNABLE.
+int
+fork(void)
+{
+  int i, pid;
+  struct proc *np;
+
+  // Allocate process.
+  if((np = allocproc()) == 0)
+    return -1;
+
+  // Copy process state from p.
+  if((np->pgdir = copyuvm(proc->pgdir, proc->sz)) == 0){
+    kfree(np->kstack);
+    np->kstack = 0;
+    np->state = UNUSED;
+    return -1;
+  }
+  np->sz = proc->sz;
+  np->tickets = proc->tickets; 
+  np->parent = proc;
+  *np->tf = *proc->tf;
+
+  /* The following code is added by haoda le and netid hxl180046
+  **when forking, the child will have the same number of tickets as its parent
+  */
+  setproctickets(np, proc->tickets);
+  /* End of code added */
+    
+    
+  // Clear %eax so that fork returns 0 in the child.
+  np->tf->eax = 0;
+
+  for(i = 0; i < NOFILE; i++)
+    if(proc->ofile[i])
+      np->ofile[i] = filedup(proc->ofile[i]);
+  np->cwd = idup(proc->cwd);
+ 
+  pid = np->pid;
+  np->state = RUNNABLE;
+  safestrcpy(np->name, proc->name, sizeof(proc->name));
+  return pid;
+}
+
+// Exit the current process.  Does not return.
+// An exited process remains in the zombie state
+// until its parent calls wait() to find out it exited.
+void
+exit(void)
+{
+  struct proc *p;
+  int fd;
+
+  if(proc == initproc)
+    panic("init exiting");
+
+  // Close all open files.
+  for(fd = 0; fd < NOFILE; fd++){
+    if(proc->ofile[fd]){
+      fileclose(proc->ofile[fd]);
+      proc->ofile[fd] = 0;
+    }
+  }
+
+  iput(proc->cwd);
+  proc->cwd = 0;
+
+  acquire(&ptable.lock);
+
+  // Parent might be sleeping in wait().
+  wakeup1(proc->parent);
+
+  // Pass abandoned children to init.
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+    if(p->parent == proc){
+      p->parent = initproc;
+      if(p->state == ZOMBIE)
+        wakeup1(initproc);
+    }
+  }
+
+
+  //added, Remove from lottery
+  setproctickets(proc, 0);
+
+
+  // Jump into the scheduler, never to return.
+  proc->state = ZOMBIE;
+  sched();
+  panic("zombie exit");
+}
+
+// Wait for a child process to exit and return its pid.
+// Return -1 if this process has no children.
+int
+wait(void)
+{
+  struct proc *p;
+  int havekids, pid;
+
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for zombie children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != proc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->state = UNUSED;
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+
+	//added
+        p->ticks = 0;
+        setproctickets(p, 0);
+
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || proc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(proc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
+
+// Per-CPU process scheduler.
+// Each CPU calls scheduler() after setting itself up.
+// Scheduler never returns.  It loops, doing:
+//  - choose a process to run
+//  - swtch to start running that process
+//  - eventually that process transfers control
+//      via swtch back to the scheduler.
+// Per-CPU process scheduler.
+void
+scheduler(void)
+{
+  struct proc *p;
+
+  for(;;){
+    // Enable interrupts on this processor.
+    sti();
+
+    acquire(&ptable.lock); // x86 এ প্রসেস টেবিল স্ক্যান করার আগে একবার লক করতে হয়
+
+    int total_tickets = 0;
+    // Step 1: Compute total tickets
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+      if(p->state == RUNNABLE) {
+        total_tickets += p->tickets;
+      }
+    }
+
+    if(total_tickets > 0) {
+      // Step 2: Draw a winning number [0, total_tickets - 1]
+      int winning = rand() % total_tickets; 
+      int current_ticket_count = 0;
+
+      // Step 3: Find the winning process and run it
+      for(p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+        if(p->state == RUNNABLE) {
+          current_ticket_count += p->tickets;
+          
+          if(current_ticket_count > winning) {
+            p->rounds++; // Increase rounds by 1
+            
+            // Switch to chosen process
+            proc = p;
+            switchuvm(p);
+            p->state = RUNNING;
+            
+            swtch(&cpu->scheduler, proc->context); // x86 context switch
+            
+            switchkvm();
+            proc = 0;
+            
+            break; // Switch to the selected process and break the inner loop
+          }
+        }
+      }
+    }
+    release(&ptable.lock);
+  }
+}
+// Enter scheduler.  Must hold only ptable.lock
+// and have changed proc->state.
+void
+sched(void)
+{
+  int intena;
+
+  if(!holding(&ptable.lock))
+    panic("sched ptable.lock");
+  if(cpu->ncli != 1)
+    panic("sched locks");
+  if(proc->state == RUNNING)
+    panic("sched running");
+  if(readeflags()&FL_IF)
+    panic("sched interruptible");
+  intena = cpu->intena;
+  swtch(&proc->context, cpu->scheduler);
+  cpu->intena = intena;
+}
+
+// Give up the CPU for one scheduling round.
+void
+yield(void)
+{
+  acquire(&ptable.lock);  //DOC: yieldlock
+    
+    
+  //added
+  if(proc->state == SLEEPING)
+    {
+      panic("Sleep happens here, apparently. We need to restore tickets.");
+    }
+    
+    
+  proc->state = RUNNABLE;
+  sched();
+  release(&ptable.lock);
+}
+
+// A fork child's very first scheduling by scheduler()
+// will swtch here.  "Return" to user space.
+void
+forkret(void)
+{
+  // Still holding ptable.lock from scheduler.
+  release(&ptable.lock);
+  
+  // Return to "caller", actually trapret (see allocproc).
+}
+
+// Atomically release lock and sleep on chan.
+// Reacquires lock when awakened.
+void
+sleep(void *chan, struct spinlock *lk)
+{
+  if(proc == 0)
+    panic("sleep");
+
+  if(lk == 0)
+    panic("sleep without lk");
+
+  // Must acquire ptable.lock in order to
+  // change p->state and then call sched.
+  // Once we hold ptable.lock, we can be
+  // guaranteed that we won't miss any wakeup
+  // (wakeup runs with ptable.lock locked),
+  // so it's okay to release lk.
+  if(lk != &ptable.lock){  //DOC: sleeplock0
+    acquire(&ptable.lock);  //DOC: sleeplock1
+    release(lk);
+  }
+
+  // Go to sleep.
+  proc->chan = chan;
+  proc->state = SLEEPING;
+    
+
+  /* The following code is added by haoda le and netid hxl180046
+  **store tickets before sleeping.
+  */
+  storetickets(proc);
+  /* End of code added */
+    
+    
+  sched();
+
+  // Tidy up.
+  proc->chan = 0;
+
+  // Reacquire original lock.
+  if(lk != &ptable.lock){  //DOC: sleeplock2
+    release(&ptable.lock);
+    acquire(lk);
+  }
+}
+
+// Wake up all processes sleeping on chan.
+// The ptable lock must be held.
+static void
+wakeup1(void *chan)
+{
+  struct proc *p;
+
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    if(p->state == SLEEPING && p->chan == chan)
+      {
+        
+	/* The following code is added by haoda le and netid hxl180046
+	**restore tickets before waking.
+	*/
+        restoretickets(p);
+	/* End of code added */
+
+        
+        p->state = RUNNABLE;
+        
+      }
+}
+
+// Wake up all processes sleeping on chan.
+void
+wakeup(void *chan)
+{
+  acquire(&ptable.lock);
+  wakeup1(chan);
+  release(&ptable.lock);
+}
+
+// Kill the process with the given pid.
+// Process won't exit until it returns
+// to user space (see trap in trap.c).
+int
+kill(int pid)
+{
+  struct proc *p;
+
+  acquire(&ptable.lock);
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+    if(p->pid == pid){
+      p->killed = 1;
+      // Wake process from sleep if necessary.
+      if(p->state == SLEEPING)
+	{
+          
+	  /* The following code is added by haoda le and netid hxl180046
+	  **restore tickets before waking
+	  */
+          restoretickets(p);
+	  /* End of code added */
+          
+          
+	  p->state = RUNNABLE;
+	}
+      release(&ptable.lock);
+      return 0;
+    }
+  }
+  release(&ptable.lock);
+  return -1;
+}
+
+// Print a process listing to console.  For debugging.
+// Runs when user types ^P on console.
+// No lock to avoid wedging a stuck machine further.
+void
+procdump(void)
+{
+  static char *states[] = {
+    [UNUSED]    "unused",
+    [EMBRYO]    "embryo",
+    [SLEEPING]  "sleep ",
+    [RUNNABLE]  "runble",
+    [RUNNING]   "run   ",
+  [ZOMBIE]    "zombie"
+  };
+  int i;
+  struct proc *p;
+  char *state;
+  uint pc[10];
+  
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+    if(p->state == UNUSED)
+      continue;
+   if(p->state >= 0 && p->state < NELEM(states) && states[p->state])
+      state = states[p->state];
+    else
+      state = "???";
+    
+    // Updated procdump for Task 5
+    cprintf("%d %s %s \t tickets: %d \t rounds: %d", p->pid, state, p->name, p->tickets, p->rounds);
+    
+    if(p->state == SLEEPING){
+      getcallerpcs((uint*)p->context->ebp+2, pc);
+      for(i=0; i<10 && pc[i] != 0; i++)
+        cprintf(" %p", pc[i]);
+    }
+    cprintf("\n");
+  }
+}
+
+
+
